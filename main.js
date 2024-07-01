86bc7450c4bb8951e850d5d6185b1a9951efdc61
T Mohamed Yaser <140265777+Yaser-123@users.noreply.github.com>
2024-07-01 18:38:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,20 @@ function validateForm() {
     return false;
   }
 }
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1800,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+AOS.init({
+  duration: 800,
+  delay: 400
+});
