97fd8a33ea887860bfc63cefc054d240088eee54
KavyaBS123 <kavyabsalawadagi@gmail.com>
2024-07-13 20:53:23 +0530

    gototop

--- script.js
+++ script.js
@@ -88,6 +88,30 @@ document
       testimonialsContainer.appendChild(testimonialDiv);
     });
 
+ // Get the button
+ let mybutton = document.getElementById("topButton");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+ 
+ function scrollFunction() {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+         mybutton.style.display = "block";
+     } else {
+         mybutton.style.display = "none";
+     }
+ }
+ 
+ // When the user clicks on the button, scroll to the top of the document
+ mybutton.addEventListener("click", function() {
+     document.body.scrollTop = 0;
+     document.documentElement.scrollTop = 0;
+ });
+ // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+ 
+ 
     // Add new random testimonials
     newTestimonials.slice(0, 3).forEach((testimonial) => {
       const testimonialDiv = document.createElement("div");
